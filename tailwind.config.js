40d14a52b8d8013b000a991d3c6d6c60c9af424c
nayem9b <razwanniam1@gmail.com>
2022-11-23 20:53:41 +0600

    all npm packages installed and project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#9a8bef",
+
+          secondary: "#52d84b",
+
+          accent: "#7686e0",
+
+          neutral: "#28343E",
+
+          "base-100": "#f3f4f6",
+
+          info: "#98BAE6",
+
+          success: "#5AEDA3",
+
+          warning: "#C48003",
+
+          error: "#F74A4D",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
