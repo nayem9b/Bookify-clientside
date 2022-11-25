badfa8766b1280cbfd40c33dc844eb6bb6aab829
nayem9b <razwanniam1@gmail.com>
2022-11-25 16:12:11 +0600

    all buyers and sellers data are loaded in admin dashboard

--- src/Components/AllBuyers/AllBuyers.js
+++ src/Components/AllBuyers/AllBuyers.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const AllBuyers = () => {
+  const {
+    data: allbuyers = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["email"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/users/buyers`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(allbuyers);
   return (
     <div>
-      <h1>This is all buyers</h1>
+      <h2 className='text-3xl'>All Buyers</h2>
+      <div className='overflow-x-auto'>
+        <table className='table w-full'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allbuyers.map((buyer, i) => (
+              <tr key={buyer._id}>
+                <th>{i + 1}</th>
+                <td>{buyer.name}</td>
+                <td>{buyer.email}</td>
+
+                <td>
+                  <button className='btn btn-error btn-sm'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Components/AllSeller/AllSeller.js
+++ src/Components/AllSeller/AllSeller.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const AllSeller = () => {
+  const {
+    data: allsellers = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["email"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/users/sellers`);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  console.log(allsellers);
   return (
     <div>
-      <h1>This is all seller</h1>
+      <h2 className='text-3xl'>All Sellers</h2>
+      <div className='overflow-x-auto'>
+        <table className='table w-full'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allsellers.map((seller, i) => (
+              <tr key={seller._id}>
+                <th>{i + 1}</th>
+                <td>{seller.name}</td>
+                <td>{seller.email}</td>
+
+                <td>
+                  <button className='btn btn-error btn-sm'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
